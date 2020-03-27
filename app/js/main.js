aa9abeddd48210e963908f264c7a9c9e4a82be61
irayarovaya <ira.glazunova1@gmail.com>
2020-03-27 21:12:59 +0200

    add 404, about, contacts, settings page

--- app/js/main.js
+++ app/js/main.js
@@ -34,6 +34,14 @@ $('.icon-th-large').on('click', function(){
   $('.icon-th-large').addClass('active');
   $('.icon-th-list').removeClass('active');
 });
+$('.product-one__tabs .tab, .settings__tabs .tab').on('click', function(event) {
+  var id = $(this).attr('data-id');
+    $('.product-one__tabs, .settings__tabs').find('.tab-item').removeClass('active-tab').hide();
+    $('.product-one__tabs .tabs, .settings__tabs .tabs').find('.tab').removeClass('active');
+    $(this).addClass('active');
+    $('#'+id).addClass('active-tab').fadeIn();
+    return false;
+  });
 
 
 $('.menu__btn').on('click', function(){
